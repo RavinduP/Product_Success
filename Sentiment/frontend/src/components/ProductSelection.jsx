e97b8ca1725422426419c31530076415e5e48ff8
Nethma Gunarathne <tnethma11@gmail.com>
2025-03-16 00:03:30 +0530

    working on uploading file part

---
+++ Sentiment/frontend/src/components/ProductSelection.jsx
@@ -0,0 +1,10 @@
+const ProductSelection = () => {
+    return (
+      <div className="container product">
+        <h2>Product Selection</h2>
+        <p>Select products for analysis.</p>
+      </div>
+    );
+  };
+  
+  export default ProductSelection;
